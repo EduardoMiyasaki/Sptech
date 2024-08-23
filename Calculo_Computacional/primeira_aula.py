fbe9955cb8b92db43bc334c6911bfad27af0a420
EduardoMiyasaki <eduardomiyasaki22@gmail.com>
2024-08-23 09:38:48 -0300

    Primeira aula de python

---
+++ Calculo_Computacional/primeira_aula.py
@@ -0,0 +1,47 @@
+import math as m # Usando as da pra apelidar um import 
+print("Hello world!!!")
+print("7","4")
+a = 2
+b = 12.5
+
+soma = a + b
+potenciacao_ao_quadrado = a ** 2
+potenciacao_ao_cubo = a ** 3
+potenciacao_a_quarta = a ** 3
+
+if(a <= 2):
+        print(potenciacao_ao_quadrado)
+        print(potenciacao_ao_cubo)
+        print(potenciacao_a_quarta)
+
+c = 4 
+d = 5
+
+c_elevado_ao_quadrado = pow(c,2)
+print(c_elevado_ao_quadrado)
+
+c_elevado_ao_cubo = pow(c,3)
+c_elevado_a_quarta = c ** 4
+c_elevado_a_d = c ** d
+
+print("c elevado ao quadrado = " , c_elevado_ao_quadrado)
+print("c elevado ao cubo = " , c_elevado_ao_cubo)
+print("c elevado a quarta = " , c_elevado_a_quarta)
+print("c elevado a d " , c_elevado_a_d)
+
+# Floor = arredonda o valor pro menor possível
+# ceil = arredonda o valor pro maior possível
+# Round = arredonda o valor pro mais próximo
+
+w = 3345.61
+
+valorMax = m.floor(w)
+valorMin = m.ceil(w)
+
+print(valorMax)
+print(valorMin)
+
+'''
+oi
+'''
+
